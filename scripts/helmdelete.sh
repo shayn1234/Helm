2dc7b53e6a4df3aafa3f61f431c1250468d82ce0
shayn1234 <shaynaeh.80@gmail.com>
2020-07-21 23:12:27 +0300

    Adding regular helm

--- scripts/helmdelete.sh
+++ scripts/helmdelete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 helm delete $1-$2 --purge
-kubectl delete namespace $1-$2
-kubectl delete clusterrolebinding $1-$2-admin-binding
+kubectl delete namespace $1
+kubectl delete clusterrolebinding $1-admin-binding
 
 
