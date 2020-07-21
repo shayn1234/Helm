96133c9e75e85092cfc038b023c1c030f5f00136
shayn1234 <shaynaeh.80@gmail.com>
2020-07-22 00:43:27 +0300

    Adding regular helm

--- scripts/helmdelete.sh
+++ scripts/helmdelete.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-helm delete $1-$2 --purge
-kubectl delete namespace $1
-kubectl delete clusterrolebinding $1-admin-binding
+helm delete $2 --purge
+
 
 
--- scripts/helminstall.sh
+++ scripts/helminstall.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-printf `pwd`
-printf "%s" "Helm install ---------------"
-kubectl create namespace $1
-kubectl create clusterrolebinding $1-admin-binding --clusterrole=cluster-admin --serviceaccount=$1:default
-helm install --name $1-$2 --namespace $1 --set nsPrefix=$1
+helm install --name $2 $1
