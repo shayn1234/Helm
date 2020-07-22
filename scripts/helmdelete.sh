8d8486d24ec216de4a819252bf51691ba351e0af
shayn1234 <shaynaeh.80@gmail.com>
2020-07-22 16:57:27 +0300

    Adding regular helm

--- scripts/helmdelete.sh
+++ scripts/helmdelete.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export KUBECONFIG=/etc/kubernetes/admin.conf
 helm delete $2 --purge
 
 
--- scripts/helminstall.sh
+++ scripts/helminstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export KUBECONFIG=/etc/kubernetes/admin.conf
-env
 helm install --name $2 $1
