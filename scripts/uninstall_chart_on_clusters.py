10eebc839bba684445390bc7678f1d5370fa1266
shayn1234 <shaynaeh.80@gmail.com>
2020-08-02 12:33:43 +0300

    Adding regular helm

---
+++ scripts/uninstall_chart_on_clusters.py
@@ -0,0 +1,8 @@
+from cloudify.workflows import ctx
+from cloudify.workflows import parameters as p
+
+for node in ctx.nodes:
+    for instance in node.instances:
+        ctx.instance.runtime_properties['package_name'] = p.package_name
+        instance.execute_operation('unhelm', kwargs={
+        })
