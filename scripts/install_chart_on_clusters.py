8f2a01beb9ac2754e59b683e2d9a1bcea9f60c02
shayn1234 <shaynaeh.80@gmail.com>
2020-08-03 21:21:10 +0300

    Adding regular helm

--- scripts/install_chart_on_clusters.py
+++ scripts/install_chart_on_clusters.py
@@ -5,14 +5,19 @@ from cloudify.state import ctx_parameters as input
 #for node in ctx.nodes:
 #    for instance in node.instances:
 #        ctx.instance.runtime_properties['package_name'] = input
-#        instance.execute_operation('unhelm.install', kwargs={})
+#        instance.execute_operation('helm.install', kwargs={})
 
 
 client = get_rest_client()
-for ni in client.node_instances.list():
-    client.node_instances.update(
-        ni.id,
-        version=ni.version,
-        runtime_properties={'package_name': input}
-    )
-    ni.execute_operation('unhelm.install', kwargs={})
+for n in client.nodes.list():
+    ctx.logger.info("node {0}".format(n.id))
+    if (n.type=='kubernetes_cluster'):
+        for ni in client.node_instances.list(node_id=n.id):
+            ctx.logger.info("node {0}".format(ni.id))
+            client.node_instances.update(
+            ni.id,
+            version=ni.version,
+            runtime_properties={'package_name': input}
+            )
+            ctx.logger.info("node {0}".format(input))
+            ni.execute_operation('helm.install', kwargs={})
