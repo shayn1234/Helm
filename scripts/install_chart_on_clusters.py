cf697d89bde403b595ad4f95c68b8c91f8ebffa8
shayn1234 <shaynaeh.80@gmail.com>
2020-08-03 17:59:43 +0300

    Adding regular helm

--- scripts/install_chart_on_clusters.py
+++ scripts/install_chart_on_clusters.py
@@ -1,12 +1,11 @@
-from cloudify.workflows import ctx
-from cloudify.decorators import workflow
+from cloudify import ctx
+from cloudify.state import ctx_parameters as input
 
-@workflow
-def install_chart_on_clusters(package_name, **kwargs):
-      ctx.logger.info("1111")
-      node = ctx.get_node('kubernetes_cluster1')
-      ctx.logger.info("22222")
-         for instance in node.instances:
-             ctx.logger.info("33333")
-                ctx.instance.runtime_properties['package_name'] = package_name
-                instance.execute_operation('helm.install', kwargs={})
+
+ctx.logger.info("1111")
+node = ctx.get_node('kubernetes_cluster1')
+ctx.logger.info("22222")
+for instance in node.instances:
+    ctx.logger.info("33333")
+    ctx.instance.runtime_properties['package_name'] = package_name
+    instance.execute_operation('helm.install', kwargs={})
