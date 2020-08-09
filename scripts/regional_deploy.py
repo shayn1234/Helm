a0e50df66c699e0715934d7494211f886481ee62
shayn1234 <shaynaeh.80@gmail.com>
2020-08-10 00:00:15 +0300

    Adding regular helm

--- scripts/regional_deploy.py
+++ scripts/regional_deploy.py
@@ -5,4 +5,4 @@ from cloudify.manager import get_rest_client
 client=get_rest_client()
 params= dict(package_name=inputs['package_name'])
 for deployment in inputs['regions']:
-    client.executions.start(deployment_id=deployment, workflow_id='install_charts_on_clusters', parameters=params)
+    client.executions.start(deployment_id=deployment, workflow_id='install_chart_on_clusters', parameters=params)
