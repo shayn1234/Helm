3688fa9806721b5d969dd97aa8c3fe5915278505
shayn1234 <shaynaeh.80@gmail.com>
2020-08-09 23:16:52 +0300

    Adding regular helm

--- scripts/regional_undeploy.py
+++ scripts/regional_undeploy.py
@@ -3,5 +3,5 @@ from cloudify.state import ctx_parameters as inputs
 from cloudify.manager import get_rest_client
 
 client=get_rest_client()
-for deployment in ctx.node.properties['regional_list']:
-    client.executions.start(deployment_id=deployment, workflow_id='uninstall_charts_on_clusters', parameters=inputs)
+for deployment in inputs['regions']:
+    client.executions.start(deployment_id=deployment, workflow_id='uninstall_charts_on_clusters', parameters=inputs['package_name'])
