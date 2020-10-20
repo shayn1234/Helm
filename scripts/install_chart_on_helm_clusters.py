54626052d86d2aed09c436f44999b2066d1a6920
shayn1234 <shaynaeh.80@gmail.com>
2020-10-20 18:07:48 +0300

    Adding regular helm

---
+++ scripts/install_chart_on_helm_clusters.py
@@ -0,0 +1,29 @@
+from cloudify.workflows import ctx
+from cloudify.manager import get_rest_client
+from cloudify.state import workflow_parameters as input
+
+#for node in ctx.nodes:
+#    for instance in node.instances:
+#        ctx.instance.runtime_properties['package_name'] = input
+#        instance.execute_operation('helm.install', kwargs={})
+
+def update_props(ni):
+	client=get_rest_client()
+	client_ni = client.node_instances.get(ni.id)
+	props = client_ni.runtime_properties
+	props['package_name'] = input['package_name']
+	client.node_instances.update(
+		client_ni.id,
+		version=client_ni.version,
+		runtime_properties=props
+	)
+
+for ni in ctx.node_instances:
+	if ni.node.type == 'kubernetes_helm_cluster':
+		update_props(ni)
+        ni.execute_operation('cloudify.interfaces.lifecycle.helm_configure', kwargs={})
+		ni.execute_operation('cloudify.interfaces.lifecycle.helm_install', kwargs={})
+
+
+
+
